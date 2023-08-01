21ae21669d175724734639f154be2352f6e65e6f
MelihEmreGuler <melihemreguler@hotmail.com>
2023-08-01 17:35:51 +0300

    add note struct

--- models/note.go
+++ models/note.go
@@ -1,1 +1,9 @@
 package models
+
+type Note struct {
+	ID        string `json:"note_id"`
+	UserId    string `json:"user_id"`
+	Title     string `json:"title"`
+	Content   string `json:"content"`
+	CreatedAt string `json:"created_at"`
+}
