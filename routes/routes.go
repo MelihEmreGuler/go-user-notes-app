07f1f71507edfc24377a668772b0f8871fbff3b7
MelihEmreGuler <melihemreguler@hotmail.com>
2023-08-04 02:08:29 +0300

    add swagger route in routes

--- routes/routes.go
+++ routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"github.com/MelihEmreGuler/go-user-notes-app/handlers"
+	"github.com/arsmn/fiber-swagger/v2"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -10,14 +11,21 @@ func Routes(app *fiber.App) {
 		return c.SendString("Hello, World 👋!")
 	})
 
-	app.Post("/signup", handlers.CreateUser) // /signup
-	app.Post("/login", handlers.SignIn)      // /login
-	app.Post("/logout", handlers.SignOut)    // /logout
-	//app.Put("/user/:id", handlers.UpdateUser)    // /user/:id
-	//app.Delete("/user/:id", handlers.DeleteUser) // /user/:id
-	app.Post("/note", handlers.CreateNote) // /note
-	//app.Get("/notes", handlers.GetNotes)         // /notes
-	//app.Get("/note/:id", handlers.GetNote)       // /note/:id
-	//app.Put("/note/:id", handlers.UpdateNote)    // /note/:id
-	//app.Delete("/note/:id", handlers.DeleteNote) // /note/:id
+	app.Get("/swagger/*", swagger.New(swagger.Config{ // /swagger/*
+		URL:         "http://localhost:8080/swagger/doc.json", //The url pointing to API definition
+		DeepLinking: false,
+		Title:       "User Notes API",
+	}))
+
+	app.Post("/signup", handlers.CreateUser)           // /signup
+	app.Post("/login", handlers.SignIn)                // /login
+	app.Post("/logout", handlers.SignOut)              // /logout
+	app.Put("/user/password", handlers.UpdatePassword) // /user/password
+	app.Put("/user/email", handlers.UpdateEmail)       // /user/email
+	app.Delete("/user", handlers.DeleteUser)           // /user
+	app.Post("/note", handlers.CreateNote)             // /note
+	app.Get("/notes", handlers.GetNotes)               // /notes
+	app.Get("/note/:id", handlers.GetNote)             // /note/:id
+	app.Put("/note/:id", handlers.UpdateNote)          // /note/:id
+	app.Delete("/note/:id", handlers.DeleteNote)       // /note/:id
 }
