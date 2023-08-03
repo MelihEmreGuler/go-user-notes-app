af14d0152a4b6a841d0059b759bac6d7141cf18f
MelihEmreGuler <melihemreguler@hotmail.com>
2023-08-04 02:03:53 +0300

    add UpdatePassword, UpdateEmail features

--- middleware/authentication/authentication.go
+++ middleware/authentication/authentication.go
@@ -14,11 +14,11 @@ import (
 func SignUp(username, email, password string) error {
 
 	//checks if the username is valid
-	if valid, err := usernameValid(username); !valid {
+	if err := usernameValid(username); err != nil {
 		return err
 	}
 	//checks if the email is valid
-	if valid, err := emailValid(email); !valid {
+	if err := emailValid(email); err != nil {
 		return err
 	}
 
@@ -68,6 +68,80 @@ func SignIn(usernameOrEmail, password string) (*models.User, error) {
 	}
 }
 
+// UpdatePassword updates the password of the user
+func UpdatePassword(usernameOrEmail, oldPassword, newPassword string) error {
+
+	var user *models.User
+	var err error
+	isUsername := true
+
+	//checks if the usernameOrEmail is email
+	if strings.Contains(usernameOrEmail, "@") {
+		isUsername = false
+	}
+
+	//brings the user from the database
+	if isUsername {
+		user, err = repository.R.SelectUserByUsername(usernameOrEmail)
+		if err != nil {
+			return err
+		}
+	} else {
+		user, err = repository.R.SelectUserByEmail(usernameOrEmail)
+		if err != nil {
+			return err
+		}
+	}
+
+	//checks if the user exists
+	if user == nil {
+		return errors.New("user does not exist")
+	}
+
+	//checks if the password is correct
+	if checkPassword(oldPassword, user.PasswordHash) {
+		fmt.Println("old password is correct for user: " + usernameOrEmail)
+
+		//hashes the new password
+		hashedNewPassword := hashPassword(newPassword)
+
+		//updates the password of the user
+		return repository.R.UpdateUserPassword(user.ID, hashedNewPassword)
+	} else {
+		return errors.New(usernameOrEmail + " password is incorrect")
+	}
+}
+
+// UpdateEmail updates the email of the user
+func UpdateEmail(userId, password, newEmail string) error {
+
+	//brings the user from the database
+	user, err := repository.R.SelectUserById(userId)
+	if err != nil {
+		return err
+	}
+
+	//checks if the user exists
+	if user == nil {
+		return errors.New("user does not exist")
+	}
+
+	//checks if the password is correct
+	if checkPassword(password, user.PasswordHash) {
+		fmt.Println("password is correct for user: " + user.Username)
+
+		//checks if the email is valid
+		if err = emailValid(newEmail); err != nil {
+			return err
+		}
+
+		//updates the email of the user
+		return repository.R.UpdateUserEmail(user.ID, newEmail)
+	} else {
+		return errors.New(user.Username + " password is incorrect")
+	}
+}
+
 // hashPassword hashes the password using sha256
 func hashPassword(password string) string {
 	hashed := sha256.Sum256([]byte(password))
@@ -86,37 +160,39 @@ func checkPassword(inputPassword, storedHashedPassword string) bool {
 	return false // password is incorrect
 }
 
-func usernameValid(username string) (bool, error) {
+// usernameValid checks if the username is valid
+func usernameValid(username string) error {
 
 	//checks if the username is empty
 	if username == "" {
-		return false, errors.New("username cannot be empty")
+		return errors.New("username cannot be empty")
 	}
 
 	//checks if the username is longer than 3 characters
 	if len(username) < 3 {
-		return false, errors.New("username must be longer than 3 characters")
+		return errors.New("username must be longer than 3 characters")
 	}
 
 	//checks if the username contains only letters and numbers
 	for _, ch := range username {
 		if !(ch >= 'a' && ch <= 'z') && !(ch >= 'A' && ch <= 'Z') && !(ch >= '0' && ch <= '9') {
-			return false, errors.New("username contains invalid characters")
+			return errors.New("username contains invalid characters")
 		}
 	}
 
-	return true, nil
+	return nil
 }
 
-func emailValid(email string) (bool, error) {
+// emailValid checks if the email is valid
+func emailValid(email string) error {
 	//checks if the email is empty
 	if email == "" {
-		return false, errors.New("email cannot be empty")
+		return errors.New("email cannot be empty")
 	}
 
 	//checks if the email is longer than 3 characters
 	if len(email) < 3 {
-		return false, errors.New("email must be longer than 3 characters")
+		return errors.New("email must be longer than 3 characters")
 	}
 
 	//checks if the email contains "@" character and only one and not at the beginning or end.
@@ -125,15 +201,15 @@ func emailValid(email string) (bool, error) {
 		if ch == '@' {
 			atCount++
 			if i == 0 || i == len(email)-1 {
-				return false, errors.New("email cannot start or end with @")
+				return errors.New("email cannot start or end with @")
 			}
 		}
 	}
 
 	//checks last 4 characters of the email for ".com"
 	if email[len(email)-4:] != ".com" {
-		return false, errors.New("email must end with .com")
+		return errors.New("email must end with .com")
 	}
 
-	return true, nil
+	return nil
 }
