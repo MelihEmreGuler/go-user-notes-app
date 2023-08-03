c308f19e1f447758208e8a9be124ac54484f03d7
MelihEmreGuler <melihemreguler@hotmail.com>
2023-08-04 02:02:00 +0300

    add endpoints and swagger comments

--- handlers/user.go
+++ handlers/user.go
@@ -4,8 +4,27 @@ import (
 	"github.com/MelihEmreGuler/go-user-notes-app/middleware/authentication"
 	"github.com/MelihEmreGuler/go-user-notes-app/middleware/session"
 	"github.com/MelihEmreGuler/go-user-notes-app/models"
+	"github.com/MelihEmreGuler/go-user-notes-app/repository"
 	"github.com/gofiber/fiber/v2"
 )
+
+type createUserRequest struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+type signInRequest struct {
+	UsernameOrEmail string `json:"username_or_email"`
+	Password        string `json:"password"`
+}
+type updatePasswordRequest struct {
+	OldPassword string `json:"old_password"`
+	NewPassword string `json:"new_password"`
+}
+type updateEmailRequest struct {
+	Password string `json:"password"`
+	Email    string `json:"email"`
+}
 
 /*curl -X POST -H "Content-Type: application/json" -d '{
   "username": "mertGuler",
@@ -15,12 +34,17 @@ import (
 */
 
 // CreateUser creates a new user
+// @Summary Create a new user
+// @Description Create a new user with the given username, email, and password
+// @Tags Users
+// @Accept json
+// @Produce json
+// @Param request body createUserRequest true "User Information"
+// @Success 200 {object} map[string]interface{}
+// @Failure 400 {object} map[string]interface{}
+// @Router /signup [post]
 func CreateUser(c *fiber.Ctx) error {
-	type createUserRequest struct {
-		Username string `json:"username"`
-		Email    string `json:"email"`
-		Password string `json:"password"`
-	}
+
 	request := &createUserRequest{}
 	if err := c.BodyParser(request); err != nil {
 		return err
@@ -42,14 +66,19 @@ func CreateUser(c *fiber.Ctx) error {
 */
 
 // SignIn signs in a user
+// @Summary Sign in a user
+// @Description Sign in a user with the given username/email and password
+// @Tags Users
+// @Accept json
+// @Produce json
+// @Param request body signInRequest true "User Login Information"
+// @Success 200 {object} map[string]interface{}
+// @Failure 400 {object} map[string]interface{}
+// @Router /login [post]
 func SignIn(c *fiber.Ctx) error {
 	var user *models.User
 	var err error
 
-	type signInRequest struct {
-		UsernameOrEmail string `json:"username_or_email"`
-		Password        string `json:"password"`
-	}
 	request := &signInRequest{}
 	if err = c.BodyParser(request); err != nil {
 		return err
@@ -71,9 +100,114 @@ func SignIn(c *fiber.Ctx) error {
 }
 
 // SignOut signs out a user
+// @Summary Sign out a user
+// @Description Sign out the currently logged-in user
+// @Tags Users
+// @Accept json
+// @Produce json
+// @Success 200 {object} map[string]interface{}
+// @Failure 400 {object} map[string]interface{}
+// @Router /logout [post]
 func SignOut(c *fiber.Ctx) error {
+	c.ClearCookie("session_id")
 	// Delete session from storage
 	if err := session.DeleteSession(c); err != nil {
+		return err
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"success": true, // Add this line to indicate successful logout
+	})
+}
+
+// UpdatePassword updates the password of a user
+// @Summary Update the password of a user
+// @Description Update the password of the currently logged-in user
+// @Tags Users
+// @Accept json
+// @Produce json
+// @Param request body updatePasswordRequest true "User Password Information"
+// @Security ApiKeyAuth
+// @Success 200 {object} map[string]interface{}
+// @Failure 400 {object} map[string]interface{}
+// @Router /user/password [put]
+func UpdatePassword(c *fiber.Ctx) error {
+
+	request := &updatePasswordRequest{}
+	if err := c.BodyParser(request); err != nil {
+		return err
+	}
+
+	// Get user from session
+	user, err := session.AuthenticateAndRefresh(c)
+	if err != nil {
+		return err
+	}
+
+	// Update password
+	if err = authentication.UpdatePassword(user.ID, request.OldPassword, request.NewPassword); err != nil {
+		return err
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"success": true, // Add this line to indicate successful password update
+	})
+}
+
+// UpdateEmail updates the email of a user
+// @Summary Update the email of a user
+// @Description Update the email of the currently logged-in user
+// @Tags Users
+// @Accept json
+// @Produce json
+// @Param request body updateEmailRequest true "User Email Information"
+// @Security ApiKeyAuth
+// @Success 200 {object} map[string]interface{}
+// @Failure 400 {object} map[string]interface{}
+// @Router /user/email [put]
+func UpdateEmail(c *fiber.Ctx) error {
+
+	request := &updateEmailRequest{}
+	if err := c.BodyParser(request); err != nil {
+		return err
+	}
+
+	// Get user from session
+	user, err := session.AuthenticateAndRefresh(c)
+	if err != nil {
+		return err
+	}
+
+	// Update email
+	if err = authentication.UpdateEmail(user.ID, request.Password, request.Email); err != nil {
+		return err
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"success": true, // Add this line to indicate successful email update
+	})
+}
+
+// DeleteUser deletes a user
+// @Summary Delete a user
+// @Description Delete the currently logged-in user
+// @Tags Users
+// @Accept json
+// @Produce json
+// @Security ApiKeyAuth
+// @Success 200 {object} map[string]interface{}
+// @Failure 400 {object} map[string]interface{}
+// @Router /user [delete]
+func DeleteUser(c *fiber.Ctx) error {
+	// Get user from session
+	user, err := session.AuthenticateAndRefresh(c)
+
+	// Delete session from storage
+	if err = session.DeleteSession(c); err != nil {
+		return err
+	}
+	// Delete user from database
+	if err = repository.R.DeleteUser(user.ID); err != nil {
 		return err
 	}
 
